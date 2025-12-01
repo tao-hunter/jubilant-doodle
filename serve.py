546b4d47f80ef802e011a68bba67d1c45ed944b1
Alexander Tereshin <31708304+teshaTe@users.noreply.github.com>
2025-12-01 14:56:47 +0000

    fixed exposed port: 8094 -> 10006 to match the docker. (#9)

--- serve.py
+++ serve.py
@@ -27,7 +27,7 @@ def get_args() -> argparse.Namespace:
     """ Function for getting arguments """
     parser = argparse.ArgumentParser()
     parser.add_argument("--host", default="0.0.0.0")
-    parser.add_argument("--port", type=int, default=8094)
+    parser.add_argument("--port", type=int, default=10006)
     return parser.parse_args()
 
 
